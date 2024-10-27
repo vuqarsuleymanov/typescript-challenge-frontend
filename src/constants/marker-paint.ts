9a83478685bcc40de5ac3d2d7538c712332aaa5b
vuqarsuleymanov <vugar@eshyft.com>
2024-10-27 23:09:38 +0100

    https://github.com/targomo/typescript-challenge-frontend/issues/8
    Now, user can see circles with different size and color depending on peopleOn property

--- src/constants/marker-paint.ts
+++ src/constants/marker-paint.ts
@@ -1,8 +1,16 @@
 import { CircleLayerSpecification } from 'maplibre-gl'
 
 export const MARKER_PAINT = {
-  'circle-color': '#000',
-  'circle-radius': ['interpolate', ['linear'], ['zoom'], 5, 1, 15, 8],
+  // 'circle-color': '#000',
+  'circle-color': [
+    'case',
+    [">", ["get", "peopleOn"], 90], "#3bb2d0",
+    [">", ["get", "peopleOn"], 60], "#e55e5e",
+    [">", ["get", "peopleOn"], 30], "#ff3bff",
+    [">", ["get", "peopleOn"], 0], "#fbb03b",
+    "#ccc"
+  ],
+  'circle-radius': ['interpolate', ['linear'], ['zoom'], 5, 1, 15, ['*', 1, ['get', 'peopleOn']]],
   'circle-stroke-opacity': 1,
   'circle-stroke-color': '#fff',
   'circle-stroke-width': ['interpolate', ['linear'], ['zoom'], 5, 0.5, 15, 2],
